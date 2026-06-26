1f5623c36a22ca4e0b7c83df7c8bacf0488dffcf
CallmeEterHist <kjls515@gmail.com>
2026-06-26 18:11:20 +0900

    [FEAT] 석민님 코드 업그레이드

---
+++ src/components/Workpspace/MyProject.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EmptyState from "../EmptyState/EmptyState"
+
+function MyProject() {
+    return (
+        <div>
+            <EmptyState />
+        </div>
+    )
+}
+
+export default MyProject
---
+++ src/components/Workpspace/UserSetting.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserSetting() {
+    return (
+        <div>
+            UserSetting
+        </div>
+    )
+}
+
+export default UserSetting
